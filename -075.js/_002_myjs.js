49abdc1002b85d88c517a5dff7105c0606959e80
Osman Onur BAŞ <onurbass.ob@gmail.com>
2023-08-01 14:19:54 +0300

    css js starter

--- -075.js/_002_myjs.js
+++
@@ -1,2 +0,0 @@
-console.log("Ben js dosyasıyım. Merhaba");
-document.write("DOM'dan size mesaj var");
--- -075.js/_003_degiskenler.js
+++
@@ -1,7 +0,0 @@
-var firstName = "Onur";
-var lastName = "Baş";
-var info = "ogrenci";
-var address = "sinop";
-
-// var result = prompt("adınızı girin");
-// document.write(firstName);
