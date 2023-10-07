3210ab5ce67373358cd123e24038f14e3b4b6846
onurbass <onurbass.ob@gmail.com>
2023-10-08 02:18:49 +0300

    u

--- BilgeAdam10Frontend/BilgeAdamO10Frontend-main/React Teory/React/rnm/src/App.jsx
+++ BilgeAdam10Frontend/BilgeAdamO10Frontend-main/React Teory/React/rnm/src/App.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 import "./App.css";
 import Char from "./pages/chars/Char";
-import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Char />
+      <div className="show-case">
+        <Char />
+      </div>
     </div>
   );
 }
